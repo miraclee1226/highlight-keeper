e98dad965db8c23974b374814ce3fe2505b66177
Lin <heron6534@gmail.com>
2025-06-20 13:11:46 +0900

    chore: Add error handling message

--- src/side-panel/modules/side-panel-renderer.js
+++ src/side-panel/modules/side-panel-renderer.js
@@ -24,7 +24,9 @@ export function createHighlightElement(highlight) {
     scrollToHighlight({
       payload: highlight.uuid,
       onSuccess: () => {},
-      onError: () => {},
+      onError: (error) => {
+        console.error(error);
+      },
     });
   });
 
