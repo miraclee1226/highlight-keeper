9914809e846cd62ec12cee7c012ff33b7d743654
Lin <heron6534@gmail.com>
2025-07-22 00:01:44 +0900

    feat: Add DomainCard component

---
+++ src/components/card/domain-card.js
@@ -0,0 +1,31 @@
+import { Component } from "../base-component.js";
+
+export class DomainCard extends Component {
+  setup() {
+    this.data = this.props;
+    this.isResult =
+      (this.data.href && !this.data.pageCount) ||
+      (this.data.text && !this.data.siteName);
+  }
+
+  template() {
+    const { favicon, siteName, domain, pageCount, href, text } = this.data;
+
+    return `
+      <div class="domain-item">
+        <img src="${favicon}" class="domain-item__favicon" alt="${siteName}" />
+        <div class="domain-item__info">
+          <div class="domain-item__name">${
+            this.isResult ? text : siteName
+          }</div>
+          <div class="domain-item__url">${this.isResult ? href : domain}</div>
+        </div>
+        ${
+          this.isResult
+            ? ""
+            : `<div class="domain-item__count">${pageCount}</div>`
+        }
+      </div>
+    `;
+  }
+}
